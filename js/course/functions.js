8e1615ecc0b582ad03c73978f4e2232de0e4fec1
AlexandreSGJR <alexandressgjr@gmail.com>
2020-11-12 20:14:59 -0300

    Adicionando icones e funcionalidade de troca de tema

---
+++ js/course/functions.js
@@ -0,0 +1,8 @@
+function changeTheme() {
+    let theme = document.getElementById('id_tema').value;
+    let menu_3d = document.getElementById('3d_menu');
+    let menu_2d = document.getElementById('2d_menu');
+
+    theme == 1 ? menu_3d.style.display = 'flex' : menu_3d.style.display = 'none';
+    theme == 2 ? menu_2d.style.display = 'flex' : menu_2d.style.display = 'none';
+}
