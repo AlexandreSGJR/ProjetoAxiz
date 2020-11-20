b25db949ba47463fd78c032c061fe20aa1d228a1
Gabriel Rosa <gabriel.melo@bandtec.com.br>
2020-11-20 15:43:58 -0300

    Adicionando api node para cadastro de usuarios

---
+++ projeto-site/config/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  development: {
+    dialect: "sqlite",
+    storage: "./db.development.sqlite"
+  },
+  test: {
+    dialect: "sqlite",
+    storage: ":memory:"
+  },
+  production: {
+    username: 'axiz-ale',
+    password: 'Ax1z_4l3',
+    database: 'bdAxiz',
+    host: 'srvaxiz.database.windows.net',
+    dialect: 'mssql',
+    xuse_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    },
+    pool: { 
+      max: 5,
+      min: 1,
+      acquire: 5000,
+      idle: 30000,
+      connectTimeout: 5000
+    }
+  }
+};
+
+ 
